0a8b5ddb36fc1b355821268449ab9ec31b3f05fd
iamNishanth <nishanthvenk68@gmail.com>
2021-10-18 17:40:27 +0530

    file refractor

--- src/components/File.js
+++ src/components/File.js
@@ -3,31 +3,19 @@ import fileIcon from "../images/file.png";
 const File = ({ file }) => {
   const extension = file.name.split(".");
 
+  console.log(fileIcon);
+
   return (
     <div className="col-4 md:col-2 flex flex-col items-center justify-center">
-      <a
-        href={file.url}
-        target="_blank"
-        rel="noreferrer"
-        className="relative h-20 md:h-32 w-20 md:w-32 flex justify-center items-center"
+      <div
+        style={{ backgroundImage: `url("${fileIcon}")` }}
+        className="h-20 md:h-32 w-20 md:w-32 grid place-items-center bg-contain bg-center bg-no-repeat cursor-pointer"
       >
-        <img
-          src={fileIcon}
-          alt="file-icon"
-          className="h-20 md:h-32 absolute top-0 z-10"
-        />
-        <h1 className="absolute z-10 text-xl text-blue-500 font-semibold">
+        <h1 className="text-md md:text-xl text-blue-500 font-semibold">
           {extension[extension.length - 1]}
         </h1>
-      </a>
-      <a
-        href={file.url}
-        target="_blank"
-        rel="noreferrer"
-        className="w-20 md:w-32 truncate text-center mt-1"
-      >
-        {file.name}
-      </a>
+      </div>
+      <p className="w-32 truncate text-center">{file.name}</p>
     </div>
   );
 };
