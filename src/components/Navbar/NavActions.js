6eb693d1516fc013091a533fb1d1d45e2f960228
iamNishanth <nishanthvenk68@gmail.com>
2021-10-22 01:36:54 +0530

    Delete files

--- src/components/Navbar/NavActions.js
+++ src/components/Navbar/NavActions.js
@@ -1,13 +1,51 @@
 import { useDashboard } from "../../contexts/DashboardContext";
+import { useAuth } from "../../contexts/AuthContext";
+import { useParams } from "react-router-dom";
+import { useFolder, ROOT_FOLDER } from "../../hooks/useFolder";
+import { database, storage } from "../../firebase";
 import { Delete, Download, FolderPlus, Upload } from "../Icons";
 import UploadFile from "../UploadFile";
 
 const NavActions = () => {
-  const { setToggleModal, selectedFile } = useDashboard();
+  const { setToggleModal, selectedFile, setSelectedFile } = useDashboard();
+  const { folderId } = useParams();
+  const { folder } = useFolder(folderId);
+  const { currentUser } = useAuth();
 
   const downloadFile = () => {
     if (selectedFile) {
       window.open(selectedFile.url);
+    }
+  };
+
+  const deleteFile = () => {
+    if (selectedFile) {
+      if (window.confirm("Are you sure?")) {
+        database.files
+          .where("name", "==", selectedFile.name)
+          .where("userId", "==", currentUser.uid)
+          .get()
+          .then((file) => {
+            file.forEach((doc) => {
+              doc.ref.delete();
+            });
+            console.log("Deleted from db");
+          });
+        const filePath =
+          folder === ROOT_FOLDER
+            ? `${folder.path.join("/")}/${selectedFile.name}`
+            : `${folder.path.join("/")}/${folder.name}/${selectedFile.name}`;
+        storage
+          .ref(`/files/${currentUser.uid}/${filePath}`)
+          .delete()
+          .then(() => {
+            console.log("Deleted from Storage");
+            setSelectedFile(null);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     }
   };
 
@@ -31,12 +69,14 @@ const NavActions = () => {
           }`}
         />
       </button>
-      <Delete
-        size={24}
-        className={`${
-          selectedFile ? "text-blue-600 cursor-pointer" : "text-gray-300"
-        }`}
-      />
+      <button onClick={deleteFile}>
+        <Delete
+          size={24}
+          className={`${
+            selectedFile ? "text-blue-600 cursor-pointer" : "text-gray-300"
+          }`}
+        />
+      </button>
     </>
   );
 };
--- src/components/Progressbar.js
+++ src/components/Progressbar.js
@@ -1,13 +1,13 @@
 const Progressbar = ({ file }) => {
   return (
-    <div className="p-3 w-full bg-gray-200 flex justify-between rounded-md">
+    <div className="p-3 mt-3 w-full bg-gray-200 flex justify-between rounded-md">
       <h1 className="truncate w-1/2">{file.name}</h1>
       <div className="w-1/2 bg-white rounded-md">
         <div
           className="bg-green-400 h-full flex justify-center rounded-md"
           style={{ width: `${Math.round(file.progress * 100)}%` }}
         >
-          {Math.round(file.progress * 100)}%
+          <span className="text-sm">{Math.round(file.progress * 100)}%</span>
         </div>
       </div>
     </div>
