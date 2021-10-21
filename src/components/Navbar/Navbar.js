9d71400ce9afb90b0cd78b70afb7db0d845ff222
iamNishanth <nishanthvenk68@gmail.com>
2021-10-21 20:34:32 +0530

    deselect file when click outside

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,15 +4,21 @@ import AddFolderModal from "../AddFolderModal";
 import { useFolder } from "../../hooks/useFolder";
 import { useParams } from "react-router-dom";
 import Breadcrumbs from "../Breadcrumbs";
+import { useDashboard } from "../../contexts/DashboardContext";
 
 const Navbar = () => {
   const { folderId } = useParams();
   const { folder } = useFolder(folderId);
+  const { setSelectedFile } = useDashboard();
 
   return (
     <div className="top-0 z-20" style={{ position: "sticky" }}>
       <header className="px-4 w-full h-11 border-b-2 border-solid border-gray-200 bg-white flex items-center justify-between">
-        <div>
+        <div
+          onClick={() => {
+            setSelectedFile(null);
+          }}
+        >
           <Link to="/" className="text-xl font-semibold">
             iCloud <span className="text-blue-600">Drive</span>
           </Link>
@@ -20,7 +26,12 @@ const Navbar = () => {
         <div className="hidden md:w-52 md:flex md:justify-between">
           <NavActions />
         </div>
-        <div className="flex items-center">
+        <div
+          className="flex items-center"
+          onClick={() => {
+            setSelectedFile(null);
+          }}
+        >
           <h1 className="text-lg">iamNishanth</h1>
         </div>
       </header>
